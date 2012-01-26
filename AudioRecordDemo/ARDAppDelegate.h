02fe1b524d50615b2a4755341368f0e7e9dff322
Douglas Hill <Douglas2967243891@googlemail.com>
2012-01-26 18:52:07 +0000

    Made stuff like I want it, rather than like the template.

--- AudioRecordDemo/ARDAppDelegate.h
+++ AudioRecordDemo/ARDAppDelegate.h
@@ -3,17 +3,12 @@
 //  AudioRecordDemo
 //
 //  Created by Douglas Hill on 26/01/2012.
-//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
 //
 
 #import <UIKit/UIKit.h>
 
-@class ARDViewController;
-
 @interface ARDAppDelegate : UIResponder <UIApplicationDelegate>
 
 @property (strong, nonatomic) UIWindow *window;
-
-@property (strong, nonatomic) ARDViewController *viewController;
 
 @end
--- AudioRecordDemo/ARDViewController.h
+++ AudioRecordDemo/ARDViewController.h
@@ -3,7 +3,6 @@
 //  AudioRecordDemo
 //
 //  Created by Douglas Hill on 26/01/2012.
-//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
 //
 
 #import <UIKit/UIKit.h>
